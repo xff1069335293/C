1ee869a72a22addd21ab577f8747331c05372d5d
曹锋 <1069335293@qq.com>
2019-10-28 23:47:02 +0800

    提交代码

---
+++ test-10-28/test-10-28/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int count = 0;
+	int ret = a ^ b;
+	while (ret)
+	{
+		ret = ret & (ret - 1);
+		count++;
+	}
+	//将两数异或在一起，求这个数二进制中1的个数
+	printf("count=%d\n", count);
+	system("pause");
+	return 0;
+}

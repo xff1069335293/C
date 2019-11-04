0da2308602e6c29bfebfa379966296bddd375b75
曹锋 <1069335293@qq.com>
2019-11-04 23:27:00 +0800

    提交代码

---
+++ test-11-04/test-11-04/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define  N 5
+#include <stdio.h>
+#include <math.h>
+//unsigned int reverse_bit(unsigned int value) 
+//{
+//	unsigned int  sum = 0;
+//	int  i;
+//	for (i = 0; i < 32; i++) 
+//	{
+//		sum += ((value >> i) & 1)*pow(2, 31 - i);
+//	}
+//	return sum;
+//}
+//
+//int main() 
+//{
+//	unsigned int i = 25;
+//	printf("%u", reverse_bit(i));
+//	system("pause");
+//	return 0;
+//}
+
+//
+//
+//void find_number(int arr[N]) {
+//	int find_number = arr[0];
+//	int i;
+//	for (i = 0; i < N; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	for (i = 1; i < N; i++) {
+//		find_number ^= arr[i];
+//	}
+//	printf("%d\n", find_number);
+//}
+//int main() {
+//	int arr[N] = { 1,1,2,2,4 };
+//	find_number(arr);
+//	system("pause");
+//	return 0;
+//}

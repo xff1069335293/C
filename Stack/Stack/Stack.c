b3bd1a3157c38409d54265b6a8bbefe4cbe1491d
曹锋 <1069335293@qq.com>
2019-12-16 23:59:17 +0800

    提交代码

---
+++ Stack/Stack/Stack.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+typedef char STDataType;
+typedef struct Stack
+{
+	STDataType* _a;
+	int _top;		// 栈顶
+	int _capacity;  // 容量 
+}Stack;
+
+// 初始化栈 
+void StackInit(Stack* ps)
+{
+	ps->_a = NULL;
+	ps->_top = 0;
+	ps->_capacity = 0;
+}
+
+// 入栈 
+void StackPush(Stack* ps, STDataType data)
+{
+	assert(ps);
+
+	if (ps->_top == ps->_capacity)
+	{
+		size_t newcapacity = ps->_capacity == 0 ? 4 : ps->_capacity * 2;
+		ps->_a = (STDataType*)realloc(ps->_a, newcapacity * sizeof(STDataType));
+		ps->_capacity = newcapacity;
+	}
+
+	ps->_a[ps->_top] = data;
+	ps->_top++;
+}
+
+// 出栈 
+void StackPop(Stack* ps)
+{
+	assert(ps && ps->_top > 0);
+	--ps->_top;
+}
+
+// 获取栈顶元素 
+STDataType StackTop(Stack* ps)
+{
+	assert(ps);
+	return ps->_a[ps->_top - 1];
+}
+
+// 获取栈中有效元素个数 
+int StackSize(Stack* ps)
+{
+	assert(ps);
+
+	return ps->_top;
+}
+
+// 检测栈是否为空，如果为空返回非零(1)，如果不为空返回0 
+int StackEmpty(Stack* ps)
+{
+	assert(ps);
+
+	return ps->_top == 0 ? 1 : 0;
+}
+
+// 销毁栈 
+void StackDestroy(Stack* ps)
+{
+	assert(ps);
+
+	free(ps->_a);
+	ps->_a = NULL;
+	ps->_top = ps->_capacity = 0;
+}
+int main() {
+	Stack ps;
+	StackInit(&ps);
+	StackPush( &ps, 1);
+	StackPush( &ps, 2);
+	StackPush( &ps, 3);
+	StackPop(&ps);
+	StackTop(&ps);
+	StackSize(&ps);
+	system("pause");
+	return 0;
+}

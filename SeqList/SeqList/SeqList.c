92477eba7bc308dfae2f21ef9c05a1feea26f221
曹锋 <1069335293@qq.com>
2020-03-23 15:47:52 +0800

    更新代码

--- SeqList/SeqList/SeqList.c
+++ SeqList/SeqList/SeqList.c
@@ -1,177 +1,157 @@
 #include"SeqList.h"
-// 基本增删查改接口
-void SeqListInit(SeqList* ps)
-{
-	assert(ps);
 
-	ps->a = NULL;
-	ps->size = 0;
-	ps->capacity = 0;
+//顺序表初始化
+void SeqListInit(SeqList* psl)
+{
+	assert(psl);
+	psl->arr = NULL;
+	psl->capacity = 0;
+	//psl->arr = (SLDataType*)malloc(sizeof(SLDataType)*(psl->capacity));
+	psl->size = 0;
 }
 
-void SeqListDestory(SeqList* ps)
+// 顺序表销毁
+void SeqListDestory(SeqList* psl) 
 {
-	assert(ps);
-	free(ps->a);
-	ps->a = NULL;
-	ps->size = ps->capacity = 0;
+	assert(psl);
+	free(psl->arr);
+	psl->arr = NULL;
+	psl->capacity = 0;
+	psl->size = 0;
 }
 
-void SeqListPrint(SeqList* ps)
+// 顺序表打印
+void SeqListPrint(SeqList* psl)
 {
-	assert(ps);
-
-	for (size_t i = 0; i < ps->size; ++i)
+	for (size_t i = 0; i<psl->size; i++)
 	{
-		printf("%d ", ps->a[i]);
-	}
 
-	printf("%\n");
+		printf("%d ",psl->arr[i]);
+	}
+	printf("\n");
 }
 
-void CheckCacpity(SeqList* ps)
+// 检查空间，如果满了，进行增容
+void CheckCapacity(SeqList* psl)
 {
-	if (ps->size == ps->capacity)
+	assert(psl);
+	if (psl->size == psl->capacity)
 	{
-		size_t newcapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
-		ps->a = (SLDateType*)realloc(ps->a, newcapacity * sizeof(SLDateType));
-		ps->capacity = newcapacity;
+		int newcapacity = psl->capacity == 0 ? 2 : 2 * psl->capacity;
+		psl->arr = (SLDataType*)realloc(psl->arr, newcapacity*sizeof(SLDataType));//注意realloc 的使用时，后面为字节数
+		psl->capacity = newcapacity;
 	}
 }
-
-void SeqListPushBack(SeqList* ps, SLDateType x)
+// 顺序表尾插
+void SeqListPushBack(SeqList* psl, SLDataType x)
 {
-	//assert(ps);
-	//CheckCacpity(ps);
-
-	//ps->a[ps->size] = x;
-	//ps->size++;
-
-	SeqListInsert(ps, ps->size, x);
+	assert(psl);
+	CheckCapacity(psl);
+	psl->arr[psl->size] = x;
+	psl->size++;
 }
 
-void SeqListPushFront(SeqList* ps, SLDateType x)
+// 顺序表尾删
+void SeqListPopBack(SeqList* psl)
 {
-	assert(ps);
+	assert(psl);
+	psl->size--;
+}
 
-	/*CheckCacpity(ps);
-
-	size_t end = ps->size;
-	while (end > 0)
+// 顺序表头插
+void SeqListPushFront(SeqList* psl, SLDataType x)
+{
+	assert(psl);
+	CheckCapacity(psl);
+	int num = psl->size++;
+	while (num)
 	{
-		ps->a[end] = ps->a[end - 1];
-		--end;
+		psl->arr[num] = psl->arr[num - 1];
+		--num;
 	}
-
-	ps->a[0] = x;
-	++ps->size;*/
-
-	SeqListInsert(ps, 0, x);
+	psl->arr[num] = x;
 }
 
-void SeqListPopFront(SeqList* ps)
-{
-	assert(ps);
-
-	//size_t start = 0;
-	//while (start < ps->size-1)
-	//{
-	//	ps->a[start] = ps->a[start + 1];
-	//	++start;
-	//}
-	//size_t start = 1;
-	//while (start < ps->size)
-	//{
-	//	ps->a[start-1] = ps->a[start];
-	//	++start;
-	//}
-
-	//--ps->size;
-	SeqListErase(ps, 0);
-}
-
-void SeqListPopBack(SeqList* ps)
-{
-	assert(ps);
-
-	//ps->a[ps->size - 1] = 0;
-	//ps->size--;
-	SeqListErase(ps, ps->size - 1);
-}
-
-int SeqListFind(SeqList* ps, SLDateType x)
-{
-	for (size_t i = 0; i < ps->size; ++i)
+// 顺序表头删
+void SeqListPopFront(SeqList* psl)
+{	
+	assert(psl);
+	size_t num = 0;
+	while (num<psl->size-1)
 	{
-		if (ps->a[i] == x)
-		{
-			return i;
-		}
+		psl->arr[num] = psl->arr[num+1]; //注意不要越界访问
+		++num;
 	}
+	psl->size--;
+}
 
-	return -1;
+// 顺序表查找
+int SeqListFind(SeqList* psl, SLDataType x)
+{
+	assert(psl);
+	size_t num = psl->size;
+	while (num >= 0 && psl->arr[num] != x)
+	{
+		--num;
+	}
+	return num;
 }
 
 // 顺序表在pos位置插入x
-void SeqListInsert(SeqList* ps, size_t pos, SLDateType x)
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x)
 {
-	assert(ps);
-	assert(pos <= ps->size);
-
-	CheckCacpity(ps);
-
-	//int end = ps->size - 1;
-	//while (end >= (int)pos)
-	//{
-	//	ps->a[end + 1] = ps->a[end];
-	//	--end;
-	//}
-
-	size_t end = ps->size;
-	while (end > pos)
+	assert(psl);
+	assert(pos < psl->size);
+	CheckCapacity(psl);
+	size_t num = psl->size++;
+	while (num > pos)
 	{
-		ps->a[end] = ps->a[end - 1];
-		--end;
+		psl->arr[num] = psl->arr[num-1];
+		--num;
 	}
+	psl->arr[pos] = x;
 
-
-	ps->a[pos] = x;
-	ps->size++;
 }
-
 // 顺序表删除pos位置的值
-void SeqListErase(SeqList* ps, size_t pos)
+void SeqListErase(SeqList* psl, size_t pos) 
 {
-	assert(ps && pos < ps->size);
-
-	//size_t start = pos;
-	//while (start < ps->size-1)
-	//{
-	//	ps->a[start] = ps->a[start + 1];
-	//	++start;
-	//}
-
-	size_t start = pos + 1;
-	while (start < ps->size)
+	assert(psl);
+	assert(pos<psl->size);
+	size_t num = pos;
+	while (num<psl->size)
 	{
-		ps->a[start - 1] = ps->a[start];
-		++start;
+		psl->arr[num] = psl->arr[num+1];
+		++num;
 	}
-
-	ps->size--;
+	psl->size--;
 }
-
-
-
-void TestSeqList(){
+void test()
+{
 	SeqList psl;
 	SeqListInit(&psl);
-	SeqListPushBack(&psl,1);
+	SeqListPushBack(&psl, 1);
 	SeqListPushBack(&psl, 2);
 	SeqListPushBack(&psl, 3);
+	SeqListPushBack(&psl, 4);
+	SeqListPrint(&psl);
 	SeqListPopBack(&psl);
-	SeqListPushFront(&psl, 6);
-	//SeqListDestory(&psl);
-	 SeqListPrint(&psl);
-
+	SeqListPrint(&psl);
+	SeqListPushFront(&psl, 0);
+	SeqListPushFront(&psl, 1);
+	SeqListPrint(&psl);
+	SeqListPopFront(&psl);
+	SeqListPopFront(&psl);
+	SeqListPrint(&psl);
+	SeqListInsert(&psl,2, 4);
+	SeqListInsert(&psl,0, -1);
+	SeqListPrint(&psl);
+	SeqListErase(&psl, 0);
+	SeqListPrint(&psl);
+	int ret = SeqListFind(&psl, 0);
+	if (ret < 0)
+		printf("没找到该数字\n");
+	else
+		printf("找到该数字,下标为-> %d\n", ret);
+	SeqListDestory(&psl);
+	SeqListPrint(&psl);
 }
--- SeqList/SeqList/SeqList.h
+++ SeqList/SeqList/SeqList.h
@@ -1,32 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
-// 基本增删查改接口
-// 顺序表初始化
 #pragma once
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-
-typedef int SLDateType;
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+typedef int SLDataType;
 typedef struct SeqList
 {
-	SLDateType* a;
-	size_t size;
-	size_t capacity; // unsigned int
+	SLDataType* arr;  //动态开辟数组
+	size_t size;     //有效数据个数
+	size_t capacity;  //容量	
 }SeqList;
 
-// 对数据的管理:增删查改 
-void SeqListInit(SeqList* ps);
-void SeqListDestory(SeqList* ps);
-
-void SeqListPrint(SeqList* ps);
-void SeqListPushBack(SeqList* ps, SLDateType x);
-void SeqListPushFront(SeqList* ps, SLDateType x);
-void SeqListPopFront(SeqList* ps);
-void SeqListPopBack(SeqList* ps);
-
+//顺序表初始化
+void SeqListInit(SeqList* psl, size_t capacity);
+// 顺序表销毁
+void SeqListDestory(SeqList* psl);
+// 顺序表打印
+void SeqListPrint(SeqList* psl);
+// 检查空间，如果满了，进行增容
+void CheckCapacity(SeqList* psl);
+// 顺序表尾插
+void SeqListPushBack(SeqList* psl, SLDataType x);
+// 顺序表尾删
+void SeqListPopBack(SeqList* psl);
+// 顺序表头插
+void SeqListPushFront(SeqList* psl, SLDataType x);
+// 顺序表头删
+void SeqListPopFront(SeqList* psl);
 // 顺序表查找
-int SeqListFind(SeqList* ps, SLDateType x);
+int SeqListFind(SeqList* psl, SLDataType x);
 // 顺序表在pos位置插入x
-void SeqListInsert(SeqList* ps, size_t pos, SLDateType x);
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x);
 // 顺序表删除pos位置的值
+void SeqListErase(SeqList* psl, size_t pos);
